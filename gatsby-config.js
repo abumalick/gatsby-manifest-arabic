e16e773685de08a7b0fab2d9c21afad55a9282a4
abumalick <abdullah.hilson@gmail.com>
2018-11-26 10:27:02 +0000

    feat: add dir and lang to manifest configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,8 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        dir: 'rtl',
+        lang: 'ar',
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
