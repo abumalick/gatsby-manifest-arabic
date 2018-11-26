277eaf6818298732ae23015c28d2e8c1e9a0e513
abumalick <abdullah.hilson@gmail.com>
2018-11-26 10:22:11 +0000

    feat: app name in arabic

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'هذا مثال',
+        short_name: 'مثال',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
@@ -27,6 +27,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }
